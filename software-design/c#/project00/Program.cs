dca5c5e3f0a696d7cf5015ff9903ddcbb78d17f0
Azadprog <rehman19942008@hotmail.com>
2020-10-24 23:13:02 +0200

    first C# project

---
+++ software-design/c#/project00/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace project00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a String");
+            Console.ReadLine();
+        }
+    }
+}
